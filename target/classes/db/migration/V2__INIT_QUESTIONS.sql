e2fc123d7c1986fdba1f6a926db91e009432373a
Paulina Zbikowska <zbikowskap16@gmail.com>
2023-01-16 13:26:48 +0100

    with screenshots

--- target/classes/db/migration/V2__INIT_QUESTIONS.sql
+++ target/classes/db/migration/V2__INIT_QUESTIONS.sql
@@ -100,10 +100,130 @@ values(
 insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
 values(
 	10,
+	'What is the default access specifiers in Java?',
+	'access within the package only',
+	'access within the class only',
+	'access by any class',
+	3,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	11,
 	'Which operator can be used to compare two values?',
 	'><',
 	'&|',
 	'==',
+	3,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	12,
+	'How is Java platform independent?',
+	'Becouse program is written in a language that is a human-readable language',
+	'Java Byte Code interpreted by a virtual machine',
+	'Both',
+	2,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	13,
+	'What do you understand by JVM?',
+	'bytecode interpreter to execute on a machine',
+	'A virtual machine that enables the computer to run the Java program',
+	'Both',
+	3,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	14,
+	'What do you understand by JRE?',
+	'It is used to provide the runtime environment, it is the implementation of JVM.',
+	'It is a software development environment kit which is used to develop Java',
+	'It is an abstract machine which provides the runtime',
+	1,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	15,
+	'What do you understand by JDK?',
+	'It is an abstract machine which provides the runtime',
+	'It is a software development environment which is used to develop Java applications',
+	'It is a bytecode interpreter to execute on a machine',
+	2,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	16,
+	'What is  JIT compiler?',
+	'compiles just in time the bytecode that have similar functionality at the same time',
+	'contains all the native methods used in the application.',
+	'runtime data area in which the memory is allocated to the objects',
+	1,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	17,
+	'What gives Java its "write once and run anywhere" nature?',
+	'JDK',
+	'Byte Code',
+	'JRE',
+	2,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	18,
+	'Java allows our java file by... ?',
+	'.java - only for save, javac.java to compile,java classname to run',
+	'.java - only for run, javac.java to compile,java classname to save',
+	'.java - only for compile, javac.java to save,java classname to run',
+	1,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	19,
+	'What is the default value of the String variables?',
+	'0',
+	'null',
+	'0.0',
+	1,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	20,
+	'What is the default value of the local variables?',
+	'0',
+	'null',
+	'local variables are not initialized to any default value',
+	3,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	21,
+	'What is the default value of the integer variables?',
+	'0',
+	'null',
+	'0.0',
+	1,
+	-1)
+;
+insert into questions(ques_id, title, optionA, optionB, optionC, ans, chose)
+values(
+	22,
+	'What is the protected access specifier in Java? CHOOSE FALSE',
+	'can be accessed by the class of the same package',
+	'can be accessed by the sub-class of this class',
+	'can be accessed within the class only',
 	3,
 	-1)
 ;
